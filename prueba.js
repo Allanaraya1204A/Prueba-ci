07608ee63fd2f3b4237591a531cc3bd7a05f674e
Allanaraya1204A <allanaraya1204@gmail.com>
2025-05-30 16:06:29 -0600

    pruebahozaok

--- prueba.js
+++ prueba.js
@@ -17,7 +17,7 @@ car3 = {
     year: 2019
 };
 
-// Function to show car details lll
+// Function to show car details lllsss
 showCarDetails(car, car2, car3);	
 
 function showCarDetails(car, car2, car3) {
