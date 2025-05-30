c05f36180f4de0745cab86461d91d5ca69c21e68
Your Name <you@example.com>
2025-05-30 13:53:32 -0600

    car

---
+++ prueba.js
@@ -0,0 +1,29 @@
+var car, car2, car3;
+
+car = {
+    make: 'Toyota',
+    model: 'Corolla',
+    year: 2020
+};
+car2 = {    
+    make: 'Honda',
+    model: 'Civic',
+    year: 2021
+};
+
+car3 = {
+    make: 'Ford',
+    model: 'Focus',
+    year: 2019
+};
+
+
+showCarDetails(car);
+
+function showCarDetails(car, car2, car3) {
+    console.log(`Make: ${car.make}, Model: ${car.model}, Year: ${car.year}`);
+    console.log(`Make: ${car2.make}, Model: ${car2.model}, Year: ${car2.year}`);
+    console.log(`Make: ${car3.make}, Model: ${car3.model}, Year: ${car3.year}`);
+    console.log('------------------------');
+}
+
